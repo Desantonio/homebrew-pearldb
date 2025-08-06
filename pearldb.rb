1c57c896c25c7406a59e45ef7c9096b9f8aaff80
Desantonio <vatsalgayakwad12@aol.com>
2025-08-07 00:11:47 +0530

    Fix install paths; add libexec support

--- pearldb.rb
+++ pearldb.rb
@@ -1,5 +1,5 @@
 class Pearldb < Formula
-  desc "Lightweight pure Perl SQL-like database"
+  desc "A pure‑Perl SQL‑like database engine"
   homepage "https://github.com/Desantonio/pearldb"
   url "https://github.com/Desantonio/pearldb/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "5c66690657ab2ae608d71ec1de4ad12a6fe7d0be760649916e6b8d59a7d9e2cb"
@@ -8,9 +8,15 @@ class Pearldb < Formula
   depends_on "perl"
 
   def install
-    bin.install "pearldb"
-    libexec.install "lib" # ← installs all your .pm modules
-    # Patch the script to include lib path
-    inreplace bin/"pearldb", /^use strict;/, "use lib \"#{libexec}/lib\";\nuse strict;"
+    bin.install "bin/pearldb"
+    libexec.install "lib"
+    # Add lib directory to PERL5LIB
+    inreplace bin/"pearldb",
+      /^use strict;/,
+      "use lib \"\#{libexec}/lib\";\nuse strict;"
+  end
+
+  test do
+    system "#{bin}/pearldb", "--version"
   end
 end
