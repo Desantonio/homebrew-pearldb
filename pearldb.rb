fbc6765ed983ab76e018bbada73e44a709e5e04f
Desantonio <vatsalgayakwad12@aol.com>
2025-08-07 00:02:34 +0530

    Fix: add lib to @INC

--- pearldb.rb
+++ pearldb.rb
@@ -1,5 +1,5 @@
 class Pearldb < Formula
-  desc "A simple SQL-like database engine written in Perl"
+  desc "Lightweight pure Perl SQL-like database"
   homepage "https://github.com/Desantonio/pearldb"
   url "https://github.com/Desantonio/pearldb/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "5c66690657ab2ae608d71ec1de4ad12a6fe7d0be760649916e6b8d59a7d9e2cb"
@@ -8,10 +8,9 @@ class Pearldb < Formula
   depends_on "perl"
 
   def install
-    bin.install "bin/pearldb"
-  end
-
-  test do
-    system "#{bin}/pearldb", "--version"
+    bin.install "pearldb"
+    libexec.install "lib" # ← installs all your .pm modules
+    # Patch the script to include lib path
+    inreplace bin/"pearldb", /^use strict;/, "use lib \"#{libexec}/lib\";\nuse strict;"
   end
 end
